c798c39cfb499254b6fddbf055bde8643e6aac9a
soumik-rickysaha <soumik.rickysaha@gmail.com>
2018-05-12 00:12:07 +0530

    Uploading Project Structure

---
+++ Way2Automation/src/main/java/SeleniumPractise/Way2Automation/App.java
@@ -0,0 +1,13 @@
+package SeleniumPractise.Way2Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
